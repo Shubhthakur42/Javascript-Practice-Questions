62d45c98c34ab2ee0e9380fd718818ebe32e7209
Shubhthakur42 <145495342+Shubhthakur42@users.noreply.github.com>
2026-06-16 20:21:55 +0530

    Day-01 Find Largest Number

---
+++ day01/LargestNumInarray.js
@@ -0,0 +1,10 @@
+function largest(arr){
+  let max=arr[0];
+  for(let i=1;i<arr.length;i++){
+    if(arr[i]>max) max=arr[i];
+  }
+  return max;
+}
+
+let arr=[1,2,3,4,5,6,7,8,9,10];
+console.log(largest(arr));
