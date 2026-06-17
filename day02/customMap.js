515b46b3279e2130be67cd4150e453c71782f980
Shubhthakur42 <145495342+Shubhthakur42@users.noreply.github.com>
2026-06-17 19:13:21 +0530

    day 02- CustomMap

---
+++ day02/customMap.js
@@ -0,0 +1,13 @@
+function customMapper(arr,fun){
+    let res=[];
+    for(let i=0;i<arr.length;i++){
+        res.push(fun(arr[i],i));
+    }
+    return res;
+}
+function print(element){
+  return 2*element;
+}
+let arr=[2,4,54,65,25,76];
+let newarr=customMapper(arr,print);
+console.log(newarr);
